3638c4a0285be3baa324c07bd555d76db37f207f
Admin <admin@Ngos-MacBook-Pro.local>
2018-10-28 08:36:21 +0700

    cbf

--- test1.js
+++ test1.js
@@ -1,19 +1,21 @@
 const request = require('request');
 
 
-function getTempByCityName(cityName){
+function getTempByCityName(cityName,callbackf){
 
     const URL = 'http://api.openweathermap.org/data/2.5/weather?appid=01cc37655736835b0b75f2b395737694&units=metric&q=';
-
-
     request(URL +cityName, (error,response,body)=>{
-        if(error) return console.log(error);
+        if(error) return callbackf(error,null);
         const obj = JSON.parse(body);
-        
-        console.log(obj.main.temp);      
+        if(!obj.main) return callbackf(new Error('cannot find city'),null);      
+        callbackf(null,obj.main.temp);
     })
     
 }
 
-getTempByCityName('HaNoi');
+// getTempByCityName('HaNoi');
 
+getTempByCityName('Tokyo',(error,result)=>{
+    if(error) return console.log(error);
+    console.log(result);
+})
